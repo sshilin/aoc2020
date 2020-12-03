3c0d94eb38addbe63ccb8ff2fc317cb438b4cd0e
sshilin <sv.shilin@gmail.com>
2020-12-03 11:09:39 +0300

    Day 3 cleanup

--- day3/day3_test.go
+++ day3/day3_test.go
@@ -14,58 +14,33 @@ type Slope struct {
 	x, y int
 }
 
-type State struct {
-	slope     Slope
-	dx, trees int
-}
-
-func evaluate(slopes []Slope) []int {
-	row := 0
-	strLen := len(input[0])
-	states := make([]*State, 0)
-	trees := make([]int, 0)
-
-	for _, slope := range slopes {
-		states = append(states, &State{slope, 0, 0})
-	}
-
-	for _, line := range input {
-		for _, state := range states {
-			if row%state.slope.y == 0 {
-				if line[state.dx] == '#' {
-					state.trees++
-				}
-				state.dx = (state.dx + state.slope.x) % strLen
+func evaluate(slope Slope) int {
+	col, trees := 0, 0
+	for row, line := range input {
+		if row%slope.y == 0 {
+			if line[col] == '#' {
+				trees++
 			}
+			col = (col + slope.x) % len(input[0])
 		}
-		row++
-	}
-
-	for _, s := range states {
-		trees = append(trees, s.trees)
 	}
 	return trees
 }
 
 func Test_part1(t *testing.T) {
-	trees := evaluate([]Slope{
-		{3, 1},
-	})
-
-	t.Log("Result:", trees[0]) // 265
+	t.Log("Result:", evaluate(Slope{3, 1})) // 265
 }
 
 func Test_part2(t *testing.T) {
-	trees := evaluate([]Slope{
+	result := 1
+	for _, slope := range []Slope{
 		{1, 1},
 		{3, 1},
 		{5, 1},
 		{7, 1},
 		{1, 2},
-	})
-	result := 1
-	for _, t := range trees {
-		result *= t
+	} {
+		result *= evaluate(slope)
 	}
 
 	t.Log("Result:", result) // 3154761400
