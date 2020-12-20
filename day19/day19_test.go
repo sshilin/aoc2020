91ba34a40703efac0f2b9f611775910114948b51
sshilin <sv.shilin@gmail.com>
2020-12-20 20:00:31 +0300

    Day 19 both parts

--- day19/day19_test.go
+++ day19/day19_test.go
@@ -11,43 +11,66 @@ import (
 
 var (
 	input = utils.ReadStrings("day19_input.txt")
-	// input = utils.ReadStrings("test.txt")
 	rules = map[string]rule{}
+	part2 = false
 )
 
 type rule struct {
 	patterns [][]string
 }
 
-func resolveRule2(r string) string {
-	fmt.Println("====> rule ", r)
-	patterns := rules[r].patterns
-	if len(patterns) == 1 && (string(patterns[0][0]) == "a" || string(patterns[0][0]) == "b") {
-		return string(patterns[0][0])
+func resolve(name string) string {
+	if rules[name].patterns[0][0] == "a" || rules[name].patterns[0][0] == "b" {
+		return rules[name].patterns[0][0]
+	}
+	var sb strings.Builder
+
+	sb.WriteString("(")
+
+	if name == "8" && part2 {
+		sb.WriteString(fmt.Sprintf("%s+", resolve("42")))
+	} else if name == "11" && part2 {
+		rule11 := make([]string, 0)
+		for i := 1; i <= 5; i++ {
+			rule11 = append(rule11, fmt.Sprintf("%s{%d}%s{%d}", resolve("42"), i, resolve("31"), i))
+		}
+		sb.WriteString(strings.Join(rule11, "|"))
+	} else {
+		subPatterns := make([]string, 0)
+		for _, pattern := range rules[name].patterns {
+			var ssb strings.Builder
+			for _, name := range pattern {
+				ssb.WriteString(resolve(name))
+			}
+			subPatterns = append(subPatterns, ssb.String())
+		}
+		sb.WriteString(strings.Join(subPatterns, "|"))
 	}
 
-	var sb strings.Builder
-	sb.WriteString("(")
-	subPatterns := make([]string, 0)
-	for _, pattern := range rules[r].patterns {
-		var ssb strings.Builder
-		for _, r := range pattern {
-			ssb.WriteString(resolveRule2(r))
-		}
-		fmt.Println("ssb:", ssb.String())
-		subPatterns = append(subPatterns, ssb.String())
-	}
-	sb.WriteString(strings.Join(subPatterns, "|"))
 	sb.WriteString(")")
+
 	return sb.String()
 }
 
-func Test_part1(t *testing.T) {
-	sum := 0
-	for _, line := range input {
-		if line == "" {
-			break
+func splitBySections() [][]string {
+	sections := make([][]string, 0)
+	section := make([]string, 0)
+	for i := 0; i < len(input); i++ {
+		if input[i] != "" {
+			section = append(section, input[i])
+		} else {
+			sections = append(sections, section)
+			section = make([]string, 0)
 		}
+	}
+	sections = append(sections, section)
+	return sections
+}
+
+func countValidMessages() int {
+	sum := 0
+	sections := splitBySections()
+	for _, line := range sections[0] {
 		line = strings.ReplaceAll(line, "\"a\"", "a")
 		line = strings.ReplaceAll(line, "\"b\"", "b")
 		parts1 := strings.Split(line, ":")
@@ -68,19 +91,20 @@ func Test_part1(t *testing.T) {
 		rules[ruleName] = rule{patterns}
 	}
 
-	res := resolveRule2("0")
+	rg := regexp.MustCompile("^" + resolve("0") + "$")
 
-	// res = strings.ReplaceAll(res, "|)", ")")
-	t.Log(res)
-
-	rg := regexp.MustCompile("^" + res + "$")
-	for i := 141; i < len(input); i++ {
-		msg := input[i]
-
+	for _, msg := range sections[1] {
 		if rg.MatchString(msg) {
 			sum++
 		}
 	}
+	return sum
+}
+func Test_part1(t *testing.T) {
+	t.Log("Result:", countValidMessages())
+}
 
-	t.Log("Result:", sum)
+func Test_part2(t *testing.T) {
+	part2 = true
+	t.Log("Result:", countValidMessages())
 }
